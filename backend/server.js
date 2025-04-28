b600bb979e0f0b78f943ef0722b604a707f0db38
Critin <shfccritin@gmail.com>
2025-04-28 14:26:16 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,8 @@ const unreadRoutes = require('./routes/unreadRoutes');
 const scheduleRoutes = require('./routes/scheduleRoutes');
 const manychatRoutes = require('./routes/manychatRoutes');
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' })); 
 
 // Kết nối MongoDB
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("✅ MongoDB connected"))
